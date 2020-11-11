72cddc6b60535f9df2e0015e0f3c05baa3102efd
EC2 Default User <ec2-user@ip-172-31-56-238.ec2.internal>
2020-11-11 17:18:28 +0000

    Initial commit

---
+++ create_table.js
@@ -0,0 +1,29 @@
+var 
+    AWS = require("aws-sdk"),                              
+    DDB = new AWS.DynamoDB({
+        apiVersion: "2012-08-10",
+        region: "us-east-1"
+    });                                           
+
+(function createADataBaseTable(){
+    var 
+        params = {
+            AttributeDefinitions: [{
+                AttributeName: "petname", 
+                AttributeType: "S"
+            }], 
+            KeySchema: [{
+                AttributeName: "petname", 
+                KeyType: "HASH"
+            }], 
+            ProvisionedThroughput: {
+                ReadCapacityUnits: 1, 
+                WriteCapacityUnits: 1
+            }, 
+            TableName: "lostcats"
+    };
+    DDB.createTable(params, function(err, data){
+        if (err) console.log(err, err.stack); // an error occurred
+        else     console.log(data);            
+    });
+})();
